e6ddd7c27740c96d9dca04123f6da514184dd4a9
anansu <44854857+anansu@users.noreply.github.com>
2021-09-26 21:15:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
 
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>      // semantic tag / from HTML5
-    );
-  }
-}
-
 class TableOfContent extends Component {
   render() {
     return (
@@ -31,22 +19,34 @@ class Content extends Component {
   render() {
     return (
       <article>
-      <h2>HTML</h2>
-      HTML is HyperText Markup Language
+      <h2>{this.props.title}</h2>
+      {this.props.desc}
       </article>
     );
   }
 }
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>      // semantic tag / from HTML5
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="For UI"></Subject> 
         <TableOfContent></TableOfContent>
-        <Content></Content>
-      </div>  // jsx 문법임. 정리정돈의 도구다.
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+      </div>  // jsx 문법임. 리액트의 컴포넌트는 기본적으로 정리정돈의 도구다.
+      // 사용자 정의 태그를 만들어냈다. 컴포넌트 마스터!
     );
   }
 }
